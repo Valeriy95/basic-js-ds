58b825dff16fc964f4e5f922723cd9656f7e697e
Valeriy95 <vmariukhnyk@gmail.com>
2023-04-14 19:29:18 +0300

    feat: решена первая задача

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,113 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+
 class BinarySearchTree {
+  constructor(value) {
+    this.data = value;
+    this.roos = null;
+    this.left = null;
+    this.right = null;
+ }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.roos;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.roos = addNode(this.roos, data);
+    function addNode (n, data) {
+      if(!n) {
+        return new BinarySearchTree(data);
+      } else if(n.data === data) {
+        return n;
+      } else if (data < n.data) {
+        n.left = addNode(n.left, data);
+      } else {
+        n.right = addNode(n.right, data);
+      }
+      return n;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return contains(this.roos, data);
+    function contains(n, data) {
+      if (!n) {
+        return false;
+      } else if(n.data === data) {
+        return true;
+      } else if (data < n.data) {
+        return contains(n.left, data);
+      } else {
+        return contains(n.right, data);
+      }
+    } 
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return search(this.roos, data); 
+    function search(n, data) {
+      if (!n) {
+        return null;
+      } else if(n.data === data) {
+        return n;
+      } else if(data < n.data) {
+        return search(n.left, data);
+      } else {
+        return search(n.right, data);
+      }
+    } 
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.roos = deleteNode(this.roos, data);
+    function deleteNode (n, data) {
+      if(!n) {
+        return null;
+      } else if (n.data < data) {
+        n.right = deleteNode(n.right, data);
+        return n; 
+      } else if (data < n.data) {
+        n.left = deleteNode(n.left, data);
+        return n;
+      } else {
+        if(!n.left && !n.right) {
+          return null;
+        } else if(!n.right) {
+          n = n.left;
+          return n;
+        } else if(!n.left) {
+          n = n.right;
+          return n;
+        } else {
+          let min = n.right;
+          while(min.left) {
+            min = min.left;
+          }
+          n.data = min.data;
+          n.right = deleteNode(n.right, min.data);
+        }
+      }
+      return n;
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let currentMin = this.roos;
+    while(currentMin.left) {
+      currentMin = currentMin.left;
+    }
+    return currentMin.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  let currentMax = this.roos;
+   while(currentMax.right) {
+    currentMax = currentMax.right;
+   }
+   return currentMax.data
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,10 +22,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
 }
+  // removeKFromList( l, k ) {
+  //   return l.filter((element) => {
+  //     if (element != k) {
+  //       return element
+  //     }
+  //   })
+
+
+
+ 
+function removeKFromList( l, k ) {
+  throw new NotImplementedError('Not implemented');
+  return l.filter((element) => {
+    if (element != k) {
+      return element
+    }
+  })
+}
+
+// function removeKFromLis (value) {
+//   // Если нет head значит список пуст.
+//   if (!this.head) {
+//     return null;
+//   }
+
+//   let deletedNode = null;
+
+//   // Если head должен быть удален, то делаем следующий узел новым head.
+//   while (this.head && this.head.value === value) {
+//     deletedNode = this.head;
+
+//     // Переназначаем следующий за head узел на новый head.
+//     this.head = this.head.next;
+//   }
+
+//   let currentNode = this.head;
+
+//   // Если следующий узел должен быть удален,
+//   // делаем узел через один, следующим для проверки.
+//   // Перебираем все узлы и удаляем их, если их значение равно указанному.
+//   if (currentNode !== null) {
+//     while (currentNode.next) {
+//       if (currentNode.next.value === value) {
+//         deletedNode = currentNode.next;
+//         // Перезаписываем, чтобы узел через один стал следующим узлом.
+//         currentNode.next = currentNode.next.next;
+//       } else {
+//         currentNode = currentNode.next;
+//       }
+//     }
+//   }
+
+//   // Проверяем, должен ли tail быть удален.
+//   // Так как, если в цикле мы удаляем последний узел,
+//   // то с предпоследнего узла убираем только ссылку на него.
+//   // Поэтому делаем проверку на его удаление с "tail".
+//   if (this.tail && this.tail.value === value) {
+//     // в данном случае currentNode это или предпоследний узел или head.
+//     this.tail = currentNode;
+//   }
+
+//   return deletedNode;
+// }
+
 
 module.exports = {
   removeKFromList
